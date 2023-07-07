646fb1af3919f0d1c0788ac758da68a1a0ea1af2
edgarAndrew <edgarcamelo03@gmail.com>
2023-07-07 21:23:51 +0530

    added production urls

--- dashboard/src/axios.js
+++ dashboard/src/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-export const booksURL = "http://127.0.0.1:5000"
-export const lendingURL = "http://127.0.0.1:5001"
-export const studentURL = "http://127.0.0.1:5002"
+//export const booksURL = "http://127.0.0.1:5000"
+export const booksURL = "https://library-books-api.vercel.app"
+//export const lendingURL = "http://127.0.0.1:5001"
+export const lendingURL = "https://library-lendings-api.vercel.app"
+//export const studentURL = "http://127.0.0.1:5002"
+export const studentURL = "https://library-students-api.vercel.app"
 
 axios.defaults.withCredentials = true
