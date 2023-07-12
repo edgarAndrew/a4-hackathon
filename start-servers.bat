2f465f704df6f28a6f79834ac9c6d851aebed560
edgarAndrew <edgarcamelo03@gmail.com>
2023-07-12 17:31:19 +0530

    changes to bash scripts

--- start-servers.bat
+++ start-servers.bat
@@ -3,24 +3,24 @@
 REM Start Server 1
 echo Starting Books Server...
 cd books
-start "" npm test
+start cmd /k npm start
 
 REM Start Server 2
 cd ..
 echo Starting Lending Server...
 cd lending
-start "" npm test
+start cmd /k npm start
 
 REM Start Server 3
 cd ..
 echo Starting Students Server...
 cd students
-start "" npm test
+start cmd /k npm start
 
 REM Start React App
 cd ..
 echo Starting React App...
 cd dashboard
-start "" npm run dev
+start cmd /k npm run dev
 
 echo All servers started.
